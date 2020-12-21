40811f6a31c5568fa65b4d253b1db17191b05dc5
nichez <nikola.panev93@gmail.com>
2020-12-21 21:23:34 +0100

    update

--- src/App.js
+++ src/App.js
@@ -17,14 +17,19 @@ function App() {
       {isAuth && (
         <div className="messageContainer">
           <Message
-            title="Message Nikola"
+            title="Message One"
             description="This is random message one."
-            user="User Nikola"
+            user="User One"
           />
           <Message
             title="Message Two"
             description="This is test message two."
             user="User Two"
+          />
+          <Message
+            title="Message Three"
+            description="This is test message three."
+            user="User Three"
           />
         </div>
       )}
